9f4c3d855482583586e2ec27ba1ffe83fabb9eed
Ivan Yastrebov <easy.quest@ya.ru>
2023-06-29 15:59:42 +0300

    Update: 29.06.2023 15:59:42

---
+++ run_onchange_before_bootstrap.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/fish
+
+echo "выполняется до установки файлов"
+
+# for i in awk basename bzip2 cat chmod cp curl cut du find grep gzip \
+# head id mkdir proot rm sed tar xargs xz wget2; do
+# if [ -z "$(command -v "$i")" ]; then
+# echo
+# echo "Утилита '${i}' не установлена. Невозможно продолжaть."
+# echo
+# exit 1
+# fi
+# done
+# unset i
+
+
+for pkg in head id mkdir proot rm sed tar xargs xz wget2 head id mkdir proot rm sed tar xargs xz wget2
+    if [ -z "$(command -v "$pkg")" ]
+        echo
+        echo "Утилита '$pkg' не установлена. Невозможно продолжaть."
+        echo
+    end
+end
