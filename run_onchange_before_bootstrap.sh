8935dccda975e110fae760d38b875238ef70664c
Yastrebov Ivan <uq12@pm.me>
2023-07-20 01:28:38 -0400

    Update: 20.07.2023 01:28:38

--- run_onchange_before_bootstrap.sh
+++ run_onchange_before_bootstrap.sh
@@ -1,4 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
+##
+echo Выполняется после установки файлов
 
-touch ~/hello_before.txt
+# for i in awk basename bzip2 cat chmod cp curl cut du find grep gzip # head id mkdir proot rm sed tar xargs xz wget2; do
+# if [ -z  ]; then
+# echo
+# echo Утилита ${i} не установлена. Невозможно продолжaть.
+# echo
+# exit 1
+# fi
+# done
+# unset i
 
+# for pkg in head id mkdir proot rm sed tar xargs xz wget2 head id mkdir proot rm sed tar xargs xz wget2
+for pkg in exa bat 
+    if [ -z  ]
+        echo
+        echo Утилита $pkg не установлена. Невозможно продолжaть.
+        echo
+	sudo pacman -S --noconfirm $pkg
+    end
+end 
