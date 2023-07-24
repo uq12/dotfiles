fb71a2c6af50dbc04b97f7233d25cb420a470c7a
Yastrebov Ivan <uq12@pm.me>
2023-07-24 14:45:40 -0400

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -169,3 +169,4 @@ direnv hook fish | source
 # set -g direnv_fish_mode eval_after_arrow # trigger direnv at prompt, and only after arrow-based directory changes before executing command
 # set -g direnv_fish_mode disable_arrow    # trigger direnv at prompt only, this is similar functionality to the original behavior
 
+mcfly init fish | source
