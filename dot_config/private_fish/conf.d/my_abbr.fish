c699131644cb8ba663b2b70b3d79dd6250713f59
Yastrebov Ivan <uq12@pm.me>
2023-07-24 10:08:14 -0400

    Update .config/fish/conf.d/my_abbr.fish

--- dot_config/private_fish/conf.d/my_abbr.fish
+++ dot_config/private_fish/conf.d/my_abbr.fish
@@ -1,1 +1,33 @@
 abbr -a --set-cursor=! -- yi 'yay -S --noconfirm !'
+abbr -a --set-cursor=! -- yi 'yay -S --noconfirm !'
+abbr -a -- foo bar
+abbr -a -- rm 'rm -Rf'
+abbr -a -- xx 'chmod +x'
+abbr -a -- f 'chezmoi edit ~/.config/fish/config.fish'
+abbr -a -- fcd 'cd ~/.config/fish/;ls'
+abbr -a -- gs lazygit
+abbr -a -- fix termux-fix-shebang
+abbr -a -- xclip termux-clipboard-set
+abbr -a -- ou termux-open-url
+abbr -a -- z chezmoi
+abbr -a -- za 'chezmoi add'
+abbr -a -- zcd 'chezmoi cd'
+abbr -a -- zl 'chezmoi list -x scripts,dirs'
+abbr -a -- ze 'chezmoi edit'
+abbr -a -- zd 'chezmoi diff'
+abbr -a --set-cursor=! -- trans trans\ :ru\ -b\ \'!\'
+abbr -a --set-cursor=! -- t trans\ :ru\ -b\ \'!\'
+abbr -a -- yc 'yadm commit -m "'
+abbr -a -- yl 'yadm list -a'
+abbr -a -- ya 'yadm add'
+abbr -a -- y yadm
+abbr -a -- gco 'git checkout'
+abbr -a --set-cursor=! -- 4DIRS for\ dir\ in\ \*/\ncd\ \$dir\n!\ncd\ ..\nend
+abbr -a --position anywhere -- i install
+abbr -a --position anywhere -- bp bpython
+abbr -a -- py python
+abbr -a --set-cursor=! -- goo google\ --lang=ru\ --stop=10\ \'!\'
+abbr -a --set-cursor=! -- gr grep\ --recursive\ --line-number\ --binary-files=without-match\ \'!\'
+abbr -a -- pt 'ptpython --vi --dark-bg'
+abbr -a --set-cursor=! -- sms termux-sms-send\ -n\ +79628611711\ \'!\'
+abbr -a --position anywhere --function last_history_item -- !!
