2c084dee0ba613e508811835aa2223cd5feb53a1
Ваше Имя <you@example.com>
2023-07-31 01:20:25 +0300

    Update .SpaceVim.d/init.toml Add bootstrap.sh

---
+++ run_onchange_before_bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+##
+echo 'Выполняется до установки файлов'
+
+# for i in awk basename bzip2 cat chmod cp curl cut du find grep gzip # head id mkdir proot rm sed tar xargs xz wget2; do
+# if [ -z  ]; then
+# echo
+# echo Утилита ${i} не установлена. Невозможно продолжaть.
+# echo
+# exit 1
+# fi
+# done
+# unset i
+
+# for pkg in head id mkdir proot rm sed tar xargs xz wget2 head id mkdir proot rm sed tar xargs xz wget2
+for pkg in exa bat direnv starship xorg-mkfontscale fd
+    if [ -z  ]
+        echo
+        echo Утилита $pkg не установлена. Невозможно продолжaть.
+        echo
+	sudo pacman -S --noconfirm $pkg
+    end
+end 
+
+if command -v rustup
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+end
