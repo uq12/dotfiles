d5705c32080b02b9dce3f6054e26cf50877ebbf5
uq12 <uq12@pm.me>
2023-07-31 02:50:30 +0000

    first

--- run_onchange_before_bootstrap.sh
+++ run_onchange_before_bootstrap.sh
@@ -1,27 +1,46 @@
-#!/usr/bin/env fish
-##
-echo 'Выполняется до установки файлов'
+#!/bin/sudo bash
 
-# for i in awk basename bzip2 cat chmod cp curl cut du find grep gzip # head id mkdir proot rm sed tar xargs xz wget2; do
-# if [ -z  ]; then
-# echo
-# echo Утилита ${i} не установлена. Невозможно продолжaть.
-# echo
-# exit 1
-# fi
-# done
-# unset i
+#Получить необходимые компоненты
+apt-get update
+apt-get install mate-desktop-environment-core mate-terminal tightvncserver dbus-x11 -y
+apt-get install xfe -y
+apt-get clean
+apt install -y xfce4 xfce4-goodies
+apt install tigervnc-standalone-server
+##Установите необходимые файлы
+#mkdir ~/.vnc
+# wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/xstartup --no-check-certificate -P ~/.vnc/
+#wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/vncserver-start --no-check-certificate -P /usr/local/bin/
+#wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Mate/vncserver-stop --no-check-certificate -P /usr/local/bin/
 
-# for pkg in head id mkdir proot rm sed tar xargs xz wget2 head id mkdir proot rm sed tar xargs xz wget2
-for pkg in exa bat direnv starship xorg-mkfontscale fd
-    if [ -z  ]
-        echo
-        echo Утилита $pkg не установлена. Невозможно продолжaть.
-        echo
-	sudo pacman -S --noconfirm $pkg
-    end
-end 
+#chmod +x ~/.vnc/xstartup
+#chmod +x /usr/local/bin/vncserver-start
+#chmod +x /usr/local/bin/vncserver-stop
 
-if command -v rustup
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-end
+
+echo " "
+echo "Теперь вы можете запустить vncserver, запустив vncserver-start"
+echo " "
+echo "Он попросит вас ввести пароль при первом запуске."
+echo " "
+echo "Сервер VNC будет запущен по адресу 127.0.0.1:5901."
+echo " "
+echo "Вы можете подключиться к этому адресу с помощью VNC Viewer, который вы предпочитаете"
+echo " "
+echo "При подключении к этому адресу откроется окно с Mate Desktop Environment."
+echo " "
+echo " "
+echo " "
+echo "Запуск vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "Чтобы убить VNC-сервер, просто запустите vncserver-stop"
+echo " "
+echo " "
+echo " "
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+#vncserver-start
