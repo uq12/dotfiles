9e80c6225697539480fc79ace0a18417d022b94b
Ваше Имя <you@example.com>
2023-08-01 09:00:30 +0300

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -154,7 +154,7 @@ alias jctl="journalctl -p 3 -xb"
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 
 
-## Run fastfetch if session is interactive
-if status --is-interactive && type -q fastfetch
-   fastfetch --load-config neofetch
-end
+# Run fastfetch if session is interactive
+# if status --is-interactive && type -q fastfetch
+   # fastfetch --load-config neofetch
+# end
