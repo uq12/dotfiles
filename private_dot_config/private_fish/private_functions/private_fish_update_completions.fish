1901e318a90664e36ca768daf11d1e37a6b38a0b
Ivan Yastrebov <easy.quest@ya.ru>
2023-08-03 04:59:57 +0300

    Update .config/fish/functions/fish_update_completions.fish

--- private_dot_config/private_fish/private_functions/private_fish_update_completions.fish
+++ private_dot_config/private_fish/private_functions/private_fish_update_completions.fish
@@ -1,11 +1,11 @@
 function fish_update_completions --description "Update man-page based completions"
-    # Don't write .pyc files, use the manpath, clean up old completions
-    # display progress.
+# Не записывайте файлы .pyc, используйте manpath, очищайте старые завершения, отображайте прогресс.
+    
     set -l update_args -B $__fish_data_dir/tools/create_manpage_completions.py --manpath --cleanup-in ~/.config/fish/generated_completions --progress $argv
     if set -l python (__fish_anypython)
         $python $update_args
     else
-        printf "%s\n" (_ "python executable not found") >&2
+        printf "%s\n" (_ "python: исполняемый файл не найден") >&2
         return 1
     end
 end
